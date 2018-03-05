3feeace1087c772c58a275d9f48554c6adf1755a
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-04 17:17:49 -0800

    Add style & resources to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ var React=require('react');
 var About= (props) => {
   return (
     <div>
-      <h2>About Component</h2>
-      <p>Welcome to About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a simple React App which can query temperature info of any city you like!</p>
+      <p>
+        Here are some of the tools I used:
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was
+              the javascript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used
+              Open Weather Map to search for weather data by city name.
+          </li>
+        </ul>
+      </p>
     </div>
 
   );
